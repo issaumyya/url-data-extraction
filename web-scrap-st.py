32e9bc6100c995f661bef5ece108e26f46ee43f0
issaumyya <70967068+issaumyya@users.noreply.github.com>
2023-06-20 18:00:56 +0530

    Update web-scrap-st.py

--- web-scrap-st.py
+++ web-scrap-st.py
@@ -4,43 +4,29 @@ import requests
 from bs4 import BeautifulSoup
 import numpy as np
 
-def scrape_url(p, q, custom_url):
+def scrape_url(custom_url):
     urll = []
-    if custom_url:
-        urll.append(custom_url)
-    else:
-        base_url = 'https://www.makaan.com/mumbai/mumbai-south-property-10046?page='
-        for i in range(p, q + 1):
-            url = base_url + str(i)
-            result = requests.get(url).text
-            soup = BeautifulSoup(result, 'html.parser')
-            pro_url = soup.find_all('div', class_='title-line')
-            for p in pro_url:
-                for a in p.find_all('a', class_='typelink', href=True):
-                    urll.append(a['href'])
+    result = requests.get(custom_url).text
+    soup = BeautifulSoup(result, 'html.parser')
+    pro_url = soup.find_all('div', class_='title-line')
+    for p in pro_url:
+        for a in p.find_all('a', class_='typelink', href=True):
+            urll.append(a['href'])
     return urll
 
 # Streamlit UI
-st.title("Scrape URLs from Makaan.com")
-st.write("Enter the starting and ending page numbers or a custom URL to scrape URLs:")
+st.title("Property Values from Custom URL")
+st.write("Enter a custom URL to scrape property values:")
 
-p = st.number_input("Enter the starting page number:", value=1, step=1)
-q = st.number_input("Enter the ending page number:", value=1, step=1)
-custom_url = st.text_input("Enter a custom URL (optional):")
+custom_url = st.text_input("Enter a custom URL:")
 
 if st.button("Scrape URLs"):
     # Scrape the URLs
-    scraped_urls = scrape_url(int(p), int(q), custom_url)
-
-    # Ask user for Excel file name
-    excel_file_name = st.text_input("Enter the Excel file name (without extension):", value="scraped_urls")
+    scraped_urls = scrape_url(custom_url)
 
     if scraped_urls:
-        # Create a DataFrame from the scraped URLs
-        df = pd.DataFrame({"URLs": scraped_urls})
-
         # Print property values for the first 100 entries
-        st.write("Property Values:")
+        st.subheader("Property Values:")
         
         bedroom = []
         sq_ft = []
@@ -58,7 +44,7 @@ if st.button("Scrape URLs"):
         sub_loc = []
         age_of_property = []
 
-        for u in df['URLs'][:100]:
+        for u in scraped_urls[:100]:
             result = requests.get(u).text
             soup = BeautifulSoup(result, 'html.parser')
 
@@ -132,10 +118,13 @@ if st.button("Scrape URLs"):
         # Display property values
         st.dataframe(property_data)
 
+        # Ask user for Excel file name
+        excel_file_name = st.text_input("Enter the Excel file name (without extension):", value="property_values")
+
         # Save DataFrame to Excel if Excel button is clicked
         if st.button("Save as Excel"):
             excel_file_path = f"{excel_file_name}.xlsx"
             property_data.to_excel(excel_file_path, index=False)
             st.success(f"Property values saved to {excel_file_path}!")
     else:
-        st.warning("No Property URLs were found.")
+        st.warning("No URLs were scraped.")
