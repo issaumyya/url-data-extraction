474478c43a9efa10172f6256329bb558bdd27cae
issaumyya <70967068+issaumyya@users.noreply.github.com>
2023-06-26 11:27:56 +0530

    Update data-scrap.py

--- data-scrap.py
+++ data-scrap.py
@@ -23,8 +23,8 @@ st.set_page_config(
 st.title("Extract Latest Real Estate!")
 
 def search_on_makaan(query):
-    query = query.lower().replace(" ", "-")  # Convert spaces to hyphens and lowercase the query
-    search_url = f"https://www.makaan.com/listings?sortBy=date-desc&listingType=buy/{query}residential-property/buy-property-in-{query}city"
+    query = query.lower()  # lowercase the query
+    search_url = f"https://www.makaan.com/{query}-residential-property/buy-property-in-{query}-city?sortBy=date-desc"
     st.write("Search URL:", search_url)  # Add this line for debugging
     return search_url
 
